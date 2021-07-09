d5c78618cdaaab66d132dcfaa8523862f4af1812
ron93 <ronmkare1@gmail.com>
2021-07-09 10:56:02 +0300

    comments

--- Repositories/IBookRepository.cs
+++ Repositories/IBookRepository.cs
@@ -8,14 +8,15 @@ namespace book_api.Repositories
 {
     public interface IBookRepository
     {
+        //get all books
         Task<IEnumerable<Book>> Get();
-        
+        //get one book
         Task<Book> Get(int id);
-
+        //create a book
         Task<Book> Create(Book book);
-
+        //update a book
         Task Update(Book book);
-
+        //delete a book
         Task Delete(int id);
         
         
