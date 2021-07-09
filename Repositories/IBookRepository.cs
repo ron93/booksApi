af72ebf29a0323afea2eb19ee415399b341eda90
ron93 <ronmkare1@gmail.com>
2021-07-09 10:53:46 +0300

    Interface defining Book db operation

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,23 @@
+using book_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_api.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetBooks();
+        
+        Task<Book> Get(int id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int id);
+        
+        
+    }
+}
