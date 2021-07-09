fd2adb50bb0ef7e639bb86118a84082d09e55be5
ron93 <ronmkare1@gmail.com>
2021-07-09 10:24:44 +0300

    Books model

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi.Models
+{
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+    }
+}
